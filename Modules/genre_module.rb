3485b04d927e6b9bb6a7a31d8ddd7c242748e997
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-21 22:03:50 +0500

    Add genre module file

---
+++ Modules/genre_module.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/music/genre'
+
+module GenreModule
+  def list_all_genres
+    if @genres.empty?
+      puts 'There are no genre'
+    else
+      @genres.each_with_index do |genre, i|
+        puts "Number: #{i + 1}, ID: #{genre.id}, Name: #{genre.name}"
+      end
+    end
+  end
+end
